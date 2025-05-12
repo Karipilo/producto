a89adfbbf40a31c050f8627b240827404bf2fdda
marceloriv <ma.riveracontreras@duocuc.com>
2025-05-11 23:10:12 -0400

    Refactor método actualizarProducto en ProductoController para mejorar la lógica de actualización y manejo de errores

--- src/main/java/com/producto/producto/controller/ProductoController.java
+++ src/main/java/com/producto/producto/controller/ProductoController.java
@@ -6,7 +6,14 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.producto.producto.model.Producto;
 import com.producto.producto.model.dto.ProductoDto;
@@ -42,6 +49,8 @@ public class ProductoController {
         return ResponseEntity.ok(productos);
     }
 
+    /*
+    ! Método que no estaba funcionando
     @PutMapping("/{nombreProducto}")
     public ResponseEntity<?> actualizarProducto(@PathVariable String nombreProducto, @Valid @RequestBody Producto producto) {
         try {
@@ -62,6 +71,23 @@ public class ProductoController {
         } catch (Exception e) {
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
                     .body(Map.of("mensaje", "Error al actualizar el producto"));
+        }
+    } */
+    @PutMapping("/{nombreProducto}")
+    public ResponseEntity<?> actualizarProducto(@PathVariable String nombreProducto, @Valid @RequestBody Producto producto) {
+        try {
+            String resultado = productoService.actualizarProducto(nombreProducto, producto);
+
+            if (resultado.equals("Producto no encontrado")) {
+                return ResponseEntity.status(HttpStatus.NOT_FOUND).body(Map.of("mensaje", "Producto no encontrado"));
+
+            }
+            return ResponseEntity.ok(Map.of("mensaje", "Producto actualizado exitosamente"));
+
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(Map.of("mensaje", "Error al actualizar el producto" + e.getMessage()));
+
         }
     }
 
